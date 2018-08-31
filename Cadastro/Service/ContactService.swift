35d162f295c6478b8e87b3d23902addbeec7dc1f
Lucas Neto Santana <lucas.a.santana@capgemini.com>
2018-08-31 12:30:49 -0300

    ajust auto-layout

--- Cadastro/Service/ContactService.swift
+++ Cadastro/Service/ContactService.swift
@@ -10,10 +10,9 @@ import Foundation
 
 class ContactService {
     
-//    static var contatos : [Contact] = []
     static var contatos : [Contact] = [
         Contact(fullname: "Lucas Neto Santana", phone: "(11) 99880-8766", mail: "lucasnetoo@hotmail.com", birthDay: Date(), genre: .male),
         Contact(fullname: "Maria Silva Santos", phone: "(11) 99880-8766", mail: "maria@hotmail.com"     , birthDay: Date(), genre: .female),
-        Contact(fullname: "João Souza da Silva", phone: "(11) 99880-8766", mail: "joao@hotmail.com", birthDay: Date(), genre: .male)
+        Contact(fullname: "João Souza da Silva", phone: "(11) 99880-8766", mail: "joao@hotmail.com"     , birthDay: Date(), genre: .male)
     ]
 }
